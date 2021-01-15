26e4501327ecb9dd7983f85c4660392cde9f5bfb
Anicet Togue <atogue@gmail.com>
2021-01-15 11:35:11 +0100

    init project

---
+++ src/main/java/org/craftchain/market/gateway/MarketCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.craftchain.market.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketCloudGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/craftchain/market/gateway/MarketCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.craftchain.market.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
