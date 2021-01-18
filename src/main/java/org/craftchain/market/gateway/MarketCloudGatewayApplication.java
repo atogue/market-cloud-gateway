b8474e33539e05d2aa8b1c7062cda96e6db4d6cb
Anicet Togue <atogue@gmail.com>
2021-01-18 19:47:25 +0100

    enable hystrix metrics

--- src/main/java/org/craftchain/market/gateway/MarketCloudGatewayApplication.java
+++ src/main/java/org/craftchain/market/gateway/MarketCloudGatewayApplication.java
@@ -3,9 +3,11 @@ package org.craftchain.market.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class MarketCloudGatewayApplication {
 
 	public static void main(String[] args) {
